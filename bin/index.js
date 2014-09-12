aaca546feb94cf9e5a23ea594280087b6b31e5dd
cutandpastey <jon@ctrlxctrlv.co.uk>
2014-09-12 11:12:08 +0100

    finished all template generation stubs

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+var spawn = require('child_process').spawn;
 var program = require('commander');
 var print = require('../lib/utils/print');
 var packageInfo = require('../package.json');
@@ -7,27 +7,55 @@ var packageInfo = require('../package.json');
 var generateNewApp = require('../lib/generators/generateNewApplicationBoilerplate');
 
 generatorMap = {
-	service: require('../lib/generators/generateNewService')
+		controller: require('../lib/generators/generateNewController'),
+		model     : require('../lib/generators/generateNewModel'),
+		service   : require('../lib/generators/generateNewService'),
+		element   : require('../lib/generators/generateNewElement'),
+		page      : require('../lib/generators/generateNewPage')
 };
 
 program
 	.version(packageInfo.version)
-	.option('-a, --application-path   [path]', 'Generate a new application boilerplate in [path]')
-	.option('-g, --generate-component [component-type] [component-name]', 'Generate a new component boilerplate')
+	.option('-g, --generate    [component-type] [component-name]', 'Generate a new app/component boilerplate')
+	.option('-c, --component   [type] [name]', 'Generate a new application boilerplate in [path] (must use -g flag)')
+	.option('-a, --application [path]', 'generate application')
+	.option('-p, --page        [name]', 'Generate a new page boilerplate (must use -g flag)')
+	.option('-w, --watch ', 'Basic proxy for npm run watch')
 	.parse(process.argv);
 
-//if we a generating a new application
-if(program.applicationPath) generateNewApp(program.applicationPath, program.args[0]);
-
-if(program.generateComponent){
-
-		var generator = generatorMap[program.generateComponent];
-
-		if(!generator){
-				print();
-				print('red', 'you cant generate a %s, mate!', program.generateComponent);
-				print();
+if(program.watch){
+		var watcher = spawn('npm', ['run', 'watch']);
+		watcher.stdout.on('data', function(data){ console.log(data.toString('utf-8')) });
+		watcher.stderr.on('data', function(data){ console.log(data.toString('utf-8')) });
+		watcher.on('close', function(code){
+				console.log('closed the process with code %s', code);
 				process.exit();
+		})
+}
+
+if(program.generate){
+
+		if(program.application){
+				generateNewApp(program.application, program.args[0]);
 		}
-		generator(program.args)
+
+		else if(program.component){
+
+				var generator = generatorMap[program.component];
+
+				if(!generator){
+						print();
+						print('red', 'you cant generate a %s, mate!', program.generateComponent);
+						print();
+						process.exit();
+				}
+				generator(program.args);
+
+		}
+
+		else if(program.page){
+				require('../lib/generators/generateNewPageGroup')([program.page]);
+		}
+
+
 }
---
+++ controller.js
@@ -0,0 +1,56 @@
+var Q = require('q');
+var React = require('react');
+var mediator = require('../mediator');
+var isNode = require('./lib/utils/isNode');
+
+
+var BaseController = function BaseController(){ this.initialize.apply(this, arguments) };
+
+BaseController.prototype = {
+
+		mediator: mediator,
+		view    : React.createClass({render: function(){ return React.DOM.br() }}),
+
+		initialize: function(){ },
+
+		render: function(){
+
+				var computedView = this.view({ model: this.model || {} })
+
+				Q()
+
+					.then(function(){
+
+							if(isNode()){ this.mediator.publish('application:render', React.renderComponentToString(computedView)) }
+							else {
+
+									React.renderComponent(
+										computedView,
+										document.querySelector('body')
+									);
+
+							}
+
+					}.bind(this))
+
+					.catch(function(e){
+
+							console.log('-------------------------------');
+							console.log('error rendering react');
+							console.log(e);
+							console.log(e.stack);
+							console.log(this);
+							console.log('-------------------------------');
+
+							this.mediator.publish('application:error', e);
+
+					}.bind(this))
+
+					.done();
+
+		},
+		clear : function(){}
+
+};
+
+module.exports = BaseController;
